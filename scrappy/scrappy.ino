9c4b7496341e31d8d87539ada8521fba07cbcbd9
cyzu <chloeyukino@gmail.com>
2018-03-01 16:14:37 +0100

    ✓ Correction de petites erreurs ✓ S'il a de la place pour touner, il tourne jusqu'a qu'il trouve de la place pour avancer (Ne tourne plus à droite et a gauche en continue -> il continue de tourner dans la même direction)

--- scrappy/scrappy.ino
+++ scrappy/scrappy.ino
@@ -17,39 +17,35 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <TimerOne.h>
 
-/*
-  Devant gauche : capteur 1
-  Gauche : capteur 2
-  devant droit : capteur 3
-  */
+const uint8_t trigPin_front_left = 3; //envoie de signal
+const uint8_t echoPin_front_left = 2; //reçoit le signal
 
-const uint8_t trigPin_forward_left = 3; //envoie de signal
-const uint8_t echoPin_forward_left = 2; //reçoit le signal
+const uint8_t trigPin_left = 9;
+const uint8_t echoPin_left = 8;
 
-const uint8_t trigPin_left = 4;
-const uint8_t echoPin_left = 5;
+const uint8_t trigPin_front_right = 6;
+const uint8_t echoPin_front_right = 7;
 
-const uint8_t trigPin_forward_right = 6;
-const uint8_t echoPin_forward_right = 7;
-
-const uint8_t trigPin_right = 9;
-const uint8_t echoPin_right = 8;
+const uint8_t trigPin_right = 4;
+const uint8_t echoPin_right = 5;
 
 const int r = 22.5 * 1.866; // Distance entre le centre du robot et capteurs
 const float teta = 30;
 const float safetyDistance = 20; // cm en fonction de la vitesse
 const float robotWidth = 20; // Hauteur 12 cm
-float h1; //espace libre à gauche 
-float h2; // espace libre à droite 
-float d1; // distances obstacles/capteurs
-float d2;
-float d3;
-float d4;
-float d_right;
-float l1;
-float l2;
-float l3;
-float l_right;
+
+float h_left; //espace libre à gauche 
+float h_right; // espace libre à droite 
+
+/*float d_front_left; // distances obstacles/capteurs
+float d_front_right;
+float d_left;
+float d_right;*/
+
+/*float l_front_left;
+float l_front_right;
+float l_left;
+float l_right;*/
 
 /* Etat initial = avancer */
 volatile int currentState = 5;
@@ -92,10 +88,10 @@ float calculDistance(uint8_t trigPin,uint8_t echoPin){
   return cm;
 }
 
-int explore(float cm_forward_left, float cm_forward_right, float cm_left, float cm_right){
+int explore(float cm_front_left, float cm_front_right, float cm_left, float cm_right){
      
   /*Serial.print("Capteur gauche : ");                               
-  Serial.print(cm_forward_left);
+  Serial.print(cm_front_left);
   Serial.print(" cm");
   Serial.println();
   Serial.print("Capteur milieu : ");      
@@ -103,60 +99,85 @@ int explore(float cm_forward_left, float cm_forward_right, float cm_left, float 
   Serial.print(" cm");
   Serial.println();
   Serial.print("Capteur droite : ");      
-  Serial.print(cm_forward_right);
+  Serial.print(cm_front_right);
   Serial.print(" cm");
   Serial.println();*/
   
-  d1=cm_forward_left;
-  d2=cm_left;
-  d3=cm_forward_right;
-  d4=cm_right;
+  /*d_front_left = cm_front_left;
+  d_front_right = cm_front_right;
+  d_left = cm_left;
+  d_right = cm_right;*/
   
   //demi distance des capteurs left & right + 21cm
-  h1 = d2 / 2 + 21;
-  h2 = d4 / 2 + 21;
+  h_left = cm_left / 2 + 21;
+  h_right = cm_right / 2 + 21;
   
-  l1 = d1 * 0.866 - 5.63;
-  l3 = d3 * 0.866 - 5.63;
-  l2 = (d3 - d1) * 0.866;
-  //l_right ???
+  /*l_front_left = d_front_left * 0.866 - 5.63;
+  l_front_right = d_front_right * 0.866 - 5.63;
+  l_left = (d_front_right - d_front_left) * 0.866;
+  //l_right ???*/
 
   Serial.print("devant gauche = ");
-  Serial.print(cm_forward_left);
+  Serial.print(cm_front_left);
   Serial.print("    devant droite = ");
-  Serial.println(cm_forward_right);
-  Serial.print("    gauche = ");
+  Serial.println(cm_front_right);
+  Serial.print("  gauche = ");
   Serial.print(cm_left);
-  Serial.print("    droit = ");
+  Serial.print("  droit = ");
   Serial.println(cm_right);
 
-   if (cm_forward_left > 20 && cm_forward_right > 20 && cm_left > 20 && cm_right > 20) return 0;
-   if (cm_left < 20) return -1;
-   if (cm_right < 20) return 1;
-   return 2;
+   if ((cm_front_right > robotWidth + safetyDistance) 
+      && (cm_front_left > robotWidth + safetyDistance) 
+      && (cm_left > robotWidth) 
+      && (cm_right > robotWidth)) {
+        // Si il y a de la place de partout, avancer
+         Serial.println("↑");
+         return 0;
+   }
+   else if (cm_front_left > robotWidth || cm_front_right > robotWidth){
+      // Si il n'y a plus de place devant mais reste de la place pour tourner, tourne dans un des côtés 
+        if (cm_left > cm_right){
+             Serial.println("←");
+             return -1;
+        }
+        else {
+             Serial.println("➝");
+             return 1;
+        }   
+   }
+   else {
+        // sinon reculer
+        Serial.println("↓");
+        return 2;
+   }
  } 
 
 void navigate()
 {
-  float cm_forward_left;  // distance of the obstacle
-  float cm_forward_right;
+  float cm_front_left;  // distance of the obstacle
+  float cm_front_right;
   float cm_left;
   float cm_right;
   
   int resultatExplore;
   
    noInterrupts();
-  cm_forward_left = calculDistance(trigPin_forward_left,echoPin_forward_left);
-  cm_forward_right = calculDistance(trigPin_forward_right,echoPin_forward_right);
+  cm_front_left = calculDistance(trigPin_front_left,echoPin_front_left);
+  cm_front_right = calculDistance(trigPin_front_right,echoPin_front_right);
   cm_left = calculDistance(trigPin_left, echoPin_left);
   cm_right = calculDistance(trigPin_right, echoPin_right);
 
-  resultatExplore=explore(cm_forward_left, cm_forward_right, cm_left, cm_right);
+  resultatExplore=explore(cm_front_left, cm_front_right, cm_left, cm_right);
   
    interrupts();
   Serial.print("Explore retourne : ");
   Serial.print(resultatExplore);
   Serial.println();
+
+  // S'il tourne, ne pas s'arrêter jusqu'a qu'il trouve de la place devant
+  if ((currentState == 1 || currentState == -1) && (resultatExplore == 1 || resultatExplore == -1)){
+    resultatExplore = currentState;
+  }
   
   if(resultatExplore != currentState){
     currentState = resultatExplore;
